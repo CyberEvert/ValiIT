bdd57a67b2b11686597d5017c8e7fc4b33601166
CyberEvert <evert.edesi@gmail.com>
2024-11-05 13:22:08 +0200

    Harjutused scanneriga

---
+++ src/daytwo/Harjutused.java
@@ -0,0 +1,82 @@
+package daytwo;
+
+public class Harjutused {
+    public static void main(String[] args) {
+        /*     String manager = "John";
+        System.out.println(manager);
+        int lengthofmanager = manager.length();
+        System.out.println(lengthofmanager);
+         */
+
+        /*
+        //Import
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int n = scanner.nextInt();
+        System.out.println(n);
+         */
+
+        //1.Hello world
+/*
+        System.out.println("Hello World");
+*/
+        //2. Simple Calculator – (Use Scanner)
+        //Task: Write a program that takes two numbers from the user and performs basic arithmetic operations
+        // (addition, subtraction, multiplication, and division).
+
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Enter the second number: ");
+        int secondNumber = scanner.nextInt();
+        System.out.println("Choose an operator: +, -, *, or /");
+        System.out.println(firstNumber + secondNumber);
+        System.out.println(firstNumber - secondNumber);
+        System.out.println(firstNumber * secondNumber);
+        System.out.println(firstNumber / secondNumber);
+         */
+
+
+        //3. Odd or Even – (Use Scanner)
+        //Task: Ask the user for a number, and display whether the number is odd or even.
+
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int number = scanner.nextInt();
+        System.out.println("  ");
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+        */
+
+        //4. Greatest Number – (Use Scanner, use if)
+        //Task:  Write a Java program that takes three numbers from the user and prints the greatest number.
+        //Test Data:
+        // Input the 1st number: 25
+        // Input the 2nd number: 78
+        // Input the 3rd number: 87
+
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1st number: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("2nd number: ");
+        int secondNumber = scanner.nextInt();
+        System.out.println("3rd number: ");
+        int thirdNumber = scanner.nextInt();
+        if (firstNumber >= secondNumber && firstNumber >= thirdNumber) {
+            System.out.println(firstNumber + " is the greatest number.");
+        } else if (secondNumber >= firstNumber && secondNumber >= thirdNumber) {
+            System.out.println(secondNumber + " is the greatest number.");
+        } else {
+            System.out.println(thirdNumber + " is the greatest number.");
+        }
+        */
+
+    }
+}
+
